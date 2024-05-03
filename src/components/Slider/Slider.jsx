dd6981b4e501ed375cee1d426707dd8831d94699
Sanyek004 <Sasha88543@gmail.com>
2024-05-03 19:04:45 +1000

    Update component Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,22 +6,19 @@ const Slider = () => {
   const slides = [
     {
       id: 1,
-      image: 'https://spaceaqua.ru/upload/resize_webp/iblock/060/188_281_140cd750bba9870f18aada2478b24840a/fdh5327l1vd011u5eaid7b18mahnaoz1.webp', // Замените на ваш путь к изображению
-      heading: 'Умягчение воды', // Измените заголовок
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna a faucibus condimentum, ipsum velit congue nisl, et fringilla eros lorem eget velit.' // Измените текст
+      image: 'https://spaceaqua.ru/upload/resize_webp/iblock/060/188_281_140cd750bba9870f18aada2478b24840a/fdh5327l1vd011u5eaid7b18mahnaoz1.webp',
+      heading: 'Коммерческие осмосы',
     },
     {
       id: 2,
-      image: 'https://spaceaqua.ru/upload/resize_webp/iblock/359/188_281_140cd750bba9870f18aada2478b24840a/839m1j3bhjquritsp8dhsirjk3j0wtlx.webp', // Замените на ваш путь к изображению
-      heading: 'Коммерческие системы', // Измените заголовок
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna a faucibus condimentum, ipsum velit congue nisl, et fringilla eros lorem eget velit.' // Измените текст
+      image: 'https://spaceaqua.ru/upload/resize_webp/iblock/359/188_281_140cd750bba9870f18aada2478b24840a/839m1j3bhjquritsp8dhsirjk3j0wtlx.webp',
+      heading: 'Для скважины и коттеджа',
     },
     {
       id: 3,
-      image: 'https://spaceaqua.ru/upload/resize_webp/iblock/087/188_281_140cd750bba9870f18aada2478b24840a/hjxbmcb4h8ety332t0mn1xkq2jux59id.webp', // Замените на ваш путь к изображению
-      heading: 'Для квартиры', // Измените заголовок
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna a faucibus condimentum, ipsum velit congue nisl, et fringilla eros lorem eget velit.' // Измените текст
-    }
+      image: 'https://spaceaqua.ru/upload/resize_webp/iblock/087/188_281_140cd750bba9870f18aada2478b24840a/hjxbmcb4h8ety332t0mn1xkq2jux59id.webp',
+      heading: 'Обезжелезивание и аэрация',
+    },
   ];
 
   const prevSlide = () => {
@@ -34,18 +31,18 @@ const Slider = () => {
 
   return (
     <div className="slider">
-      <div className="slide" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
-        {slides.map(slide => (
-          <div key={slide.id} className="card">
-            <img src={slide.image} alt={slide.heading} />
-            <h3>{slide.heading}</h3>
-            <p>{slide.text}</p>
+      <div className="slide-container">
+        <div className="slide">
+          <div className="slide-content">
+            <div className="slide-id">{slides[currentSlide].id}</div>
+            <img src={slides[currentSlide].image} alt={slides[currentSlide].heading} />
+            <h3>{slides[currentSlide].heading}</h3>
           </div>
-        ))}
+        </div>
       </div>
       <div className="navigation">
-        <button className="prev" onClick={prevSlide}>Previous</button>
-        <button className="next" onClick={nextSlide}>Next</button>
+        <button className="prev" onClick={prevSlide}>&#8249;</button>
+        <button className="next" onClick={nextSlide}>&#8250;</button>
       </div>
     </div>
   );
