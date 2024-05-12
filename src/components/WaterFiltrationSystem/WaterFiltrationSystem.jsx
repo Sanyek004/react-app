be854fc8070c37c39b10a594dec352026948cba9
Sanyek004 <Sasha88543@gmail.com>
2024-05-12 18:19:22 +1000

    Update component WaterFiltrationSystem

--- src/components/WaterFiltrationSystem/WaterFiltrationSystem.jsx
+++ src/components/WaterFiltrationSystem/WaterFiltrationSystem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Avatar } from 'antd';
-import { TwitterOutlined, InstagramOutlined, FacebookOutlined } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 import './WaterFiltrationSystem.css';
 
 const WaterFiltrationSystem = () => {
@@ -15,10 +15,14 @@ const WaterFiltrationSystem = () => {
       </div>
       <div className="image-container">
         <img src="https://spaceaqua.ru/upload/medialibrary/6eb/sgc7obfse0ku87jp256gak6lmb2an6is.png" alt="Water Filtration System" />
-        <div className="social-icons">
-          <Avatar size={32} icon={<TwitterOutlined />} className="social-icon twitter-icon" />
-          <Avatar size={32} icon={<InstagramOutlined />} className="social-icon instagram-icon" />
-          <Avatar size={32} icon={<FacebookOutlined />} className="social-icon facebook-icon" />
+        <div className="plus-icon plus-icon-1">
+          <Avatar size={32} icon={<PlusOutlined />} />
+        </div>
+        <div className="plus-icon plus-icon-2">
+          <Avatar size={32} icon={<PlusOutlined />} />
+        </div>
+        <div className="plus-icon plus-icon-3">
+          <Avatar size={32} icon={<PlusOutlined />} />
         </div>
       </div>
     </div>
