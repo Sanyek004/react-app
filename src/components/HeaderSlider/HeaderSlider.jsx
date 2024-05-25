aa59f8811eb837fb9c76487544275bc6248198cf
Sanyek004 <Sasha88543@gmail.com>
2024-05-25 14:02:58 +1000

    Update component HeaderSlider: Added possibility to pause video

--- src/components/HeaderSlider/HeaderSlider.jsx
+++ src/components/HeaderSlider/HeaderSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Spin } from 'antd';
 import { CSSTransition } from 'react-transition-group';
 import SliderControls from './SliderControls';
@@ -6,17 +6,17 @@ import './HeaderSlider.css';
 
 const slides = [
   {
-    title: 'Компактные системы AquaSmart',
-    description: 'С французским клапаном распределения потоков воды',
-    mediaUrl: 'https://spaceaqua.ru/upload/resize_webp/iblock/529/400_400_140cd750bba9870f18aada2478b24840a/sl00lg3gylyr0bxooup7b2nq0c0l40b4.webp',
-    mediaType: 'image',
+    title: 'Выставка AquaTherm Moscrow 2024',
+    description: 'В феврале прошла международная выставка AquaTher, Moscow 2024, на которой было представлено оборудование бренда SpaceAqua',
+    mediaUrl: 'https://spaceaqua.ru/upload/iblock/bc4/wrmuzunpfxk0vmbrfinj0iljh0suzepb.mp4',
+    mediaType: 'video',
     detailsUrl: 'https://example.com/details1'
   },
   {
-    title: 'Бесплатный подбор оборудования', 
-    description: 'Подберем оборудование индивидуально для вашего дома, квартиры, дачи',
-    mediaUrl: 'https://spaceaqua.ru/upload/resize_webp/iblock/13c/400_400_140cd750bba9870f18aada2478b24840a/loh4hpi3nds9fgacdatq06xarnjhb517.webp',
-    mediaType: 'image',
+    title: 'SoftX - сильнокислотная ионообменная смола', 
+    description: 'Теперь доступная фасовка на 12 литров',
+    mediaUrl: 'https://spaceaqua.ru/upload/iblock/f98/6u2b0zkafdx2cmsv1fvryexq82o7sytd.mp4',
+    mediaType: 'video',
     detailsUrl: 'https://example.com/details2'
   },
   {
@@ -25,6 +25,20 @@ const slides = [
     mediaUrl: 'https://spaceaqua.ru/upload/iblock/c86/f3japdi2b1pl88w1bathbg0ko5i5xgr5.mov',
     mediaType: 'video',
     detailsUrl: 'https://example.com/details3'
+  },
+  {
+    title: 'Бесплатный подбор оборудования',
+    description: 'Подберем оборудование индивидуально для вашего дома, квартиры, дачи', 
+    mediaUrl: 'https://spaceaqua.ru/upload/resize_webp/iblock/13c/400_400_140cd750bba9870f18aada2478b24840a/loh4hpi3nds9fgacdatq06xarnjhb517.webp',
+    mediaType: 'image',
+    detailsUrl: 'https://example.com/details3'
+  },
+  {
+    title: 'Компактные системы AquaSmart',
+    description: 'С французским клапаном распределения потоков воды', 
+    mediaUrl: 'https://spaceaqua.ru/upload/resize_webp/iblock/529/400_400_140cd750bba9870f18aada2478b24840a/sl00lg3gylyr0bxooup7b2nq0c0l40b4.webp',
+    mediaType: 'image',
+    detailsUrl: 'https://example.com/details3'
   }
 ];
 
@@ -32,6 +46,7 @@ const HeaderSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [videoLoaded, setVideoLoaded] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
+  const videoRef = useRef(null);
 
   const handlePrevClick = () => {
     handleSlideChange(currentIndex - 1);
@@ -51,6 +66,16 @@ const HeaderSlider = () => {
 
   const handleVideoLoad = () => {
     setVideoLoaded(true);
+  };
+
+  const handleVideoClick = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
   };
 
   return (
@@ -74,7 +99,7 @@ const HeaderSlider = () => {
           <SliderControls 
             onPrevClick={handlePrevClick} 
             onNextClick={handleNextClick} 
-            currentSlide={currentIndex}  // Corrected prop name
+            currentSlide={currentIndex} 
             totalSlides={slides.length} 
           />
         </div>
@@ -101,11 +126,13 @@ const HeaderSlider = () => {
                   </div>
                 )}
                 <video
+                  ref={videoRef}
                   src={slides[currentIndex].mediaUrl}
                   onLoadedData={handleVideoLoad}
                   loop
                   autoPlay
                   className="header-slider__media-item"
+                  onClick={handleVideoClick}
                 />
               </>
             )}
