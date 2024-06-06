4a8d1a325f096bff382791b0a416f379f5585023
Sanyek004 <Sasha88543@gmail.com>
2024-06-06 16:43:55 +1000

    Adding libraries: emailjs-com and react-modal. Adding a form for filling out a phone request (ContactInfo)

--- src/components/Header/ContactInfo.jsx
+++ src/components/Header/ContactInfo.jsx
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import Modal from 'react-modal';
 import './ContactInfo.css';
 
 const ContactInfo = () => {
-    return (
-        <div className="contact-info transparent">
-            <div className="contacts">
-                <a href="tel:+78005006571" className="white-text">+7 (800) 500-65-71</a>
-                <a href="mailto:mail@spaceaqua.ru" className="white-text">mail@spaceaqua.ru</a>
-            </div>
-            <button className="call-button white-text">Заказать звонок</button>
-        </div>
-    );
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (name && phone) {
+      emailjs.sendForm('service_btqzpyu', 'template_kprl7x2', e.target, 'eGFCk9XB9131pv1m4')
+        .then((result) => {
+          setMessage('Заявка отправлена');
+        }, (error) => {
+          setMessage('Ошибка отправки заявки');
+        });
+      e.target.reset();
+      setModalIsOpen(false);
+    } else {
+      setMessage('Пожалуйста, заполните все поля');
+    }
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div className="contact-info transparent">
+      <button onClick={openModal} className="call-button white-text">Заказать звонок</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Заказать звонок"
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <h2>Заказать звонок</h2>
+        <form onSubmit={sendEmail}>
+          <div>
+            <label>ФИО</label>
+            <input 
+              type="text" 
+              name="name" 
+              placeholder="Как вас зовут?" 
+              onChange={(e) => setName(e.target.value)} 
+            />
+          </div>
+          <div>
+            <label>Телефон</label>
+            <input 
+              type="text" 
+              name="phone" 
+              placeholder="+7 (___) ___-__-__" 
+              onChange={(e) => setPhone(e.target.value)} 
+            />
+          </div>
+          <button type="submit" className="call-button">Отправить</button>
+        </form>
+        {message && <p>{message}</p>}
+      </Modal>
+    </div>
+  );
 };
 
 export default ContactInfo;
