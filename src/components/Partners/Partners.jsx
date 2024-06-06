0a1c5fda4f499fdecb6c84057a06529f6bf0f5fe
Sanyek004 <Sasha88543@gmail.com>
2024-06-06 12:33:58 +1000

    Update component Partners: The distance between two containers is 50 percent

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -7,10 +7,14 @@ const Partners = () => {
 
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
-      <div style={{ marginRight: '20px' }}>
+      <div style={{ width: '50%', marginRight: '20px' }}>
         <h1>Станьте партнёром компании</h1>
       </div>
-      <div>
+      <div style={{ 
+        width: '50%',
+        padding: '20px',
+        borderRadius: '8px',
+      }}>
         <p>
           Более 5000 партнёров уже зарабатывают вместе с нами. С
           высококачественным оборудованием и гарантией на наши системы
